d473d7b63478c7375c410fe9e0396672eeeb6953
mabelKruger <mabelksouza@gmail.com>
2024-11-26 10:02:43 -0300

    add script and config of vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/project/', // Substitua 'project' pelo nome exato da subpasta do GitHub Pages
- plugins: [react()],
+  plugins: [react()],
+  base: '/project/',
 });
